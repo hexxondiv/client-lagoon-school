8b2c4d096bff76370ce719d4d5bed119c4216240
James <james@winexviv.com>
2022-07-29 08:58:32 +0100

    fix: justify paragraph in primary school page

--- src/Components/Pages/Sub-pages/Academics/PrimarySchool.js
+++ src/Components/Pages/Sub-pages/Academics/PrimarySchool.js
@@ -112,7 +112,7 @@ export default function PrimarySchool() {
                   }
                   alt=""
                 />
-                <p>{title}</p>
+                <p style={{textAlign:"justify"}}>{title}</p>
               </div>
             </div>
           );
